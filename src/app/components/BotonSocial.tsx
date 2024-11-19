5ab38811bce50e05d7af85321a61ad4d63d2de93
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-19 16:32:31 -0300

    yo naci

---
+++ src/app/components/BotonSocial.tsx
@@ -0,0 +1,22 @@
+// src/components/MyComponent.tsx
+import React from "react";
+
+interface MyComponentProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const BotonSocial: React.FC<MyComponentProps> = ({ href, children }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      type="button"
+      className="text-orange-700 border border-orange-700 hover:bg-orange-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-orange-500 dark:text-orange-500 dark:hover:text-white dark:focus:ring-orange-800 dark:hover:bg-orange-500"
+    >
+      {children}
+    </a>
+  );
+};
+
+export default BotonSocial;
