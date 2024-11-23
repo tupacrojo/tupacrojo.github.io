add78ceded17804645835f46ba2ebfe762392e27
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-11-23 13:31:36 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio Tupac",
+  title: "Portafolio Tupac",
   description: "Tupac necesita un portfolio, y hago uno",
 };
 
