df4700caadf7c5c6635fb6837e61b4204a028e75
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2024-12-19 21:24:23 -0300

    Update CardWork.tsx

--- src/app/components/CardWork.tsx
+++ src/app/components/CardWork.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const CardWork: React.FC<CardProps> = ({ title, content, footer }) => {
   return (
-    <div className="flex flex-col h-auto w-96 rounded-xl justify-between bg-white bg-opacity-10 p-4 space-y-4">
+    <div className="flex flex-col h-auto w-96 rounded-xl justify-between bg-white bg-opacity-10 p-4 space-y-4 shadow shadow-orange-500">
       <p className="flex text-2xl justify-center font-semibold">{title}</p>
       <div className="bg-white bg-opacity-5 p-2 rounded-xl">{content}</div>
       {footer && <div className="flex justify-center">{footer}</div>}
