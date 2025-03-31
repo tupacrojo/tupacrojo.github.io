50bc5c4fba6ad38c4c715108ac5f2cb63ab603e0
tupacrojo <72628251+tupacrojo@users.noreply.github.com>
2025-03-30 22:58:59 -0300

    Update QuienSoy.tsx
    el reproductor no se usa hasta que no este el nuevo audio

--- src/app/components/QuienSoy.tsx
+++ src/app/components/QuienSoy.tsx
@@ -7,7 +7,7 @@ function QuienSoy() {
     <div className="flex flex-col items-center w-5/6 lg:w-2/5 rounded-xl space-y-2">
       <div className="flex flex-row items-center space-x-3">
         <h2 className="text-3xl lg:text-6xl">Soy</h2>
-        //<ReproductorAudio src="../../SoyTupac2.wav" />
+        {/*<ReproductorAudio src="../../SoyTupac2.wav" />*/}
       </div>
       <div className="flex flex-col space-y-2 text-base lg:text-2xl">
         <TextoResaltado
